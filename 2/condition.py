4956caa8914bc86115dac992af91b59271fa228d
salman <salmanmdsultan@gmail.com>
2023-08-23 22:54:47 +0600

    Conditional basic done

--- 2/condition.py
+++ 2/condition.py
@@ -1,9 +1,19 @@
 name = input("enter your name:")
+
 email = input("enter your email:")
+phone = input("enter your phone: ")
 
 size_name = len(name)
 size_email = len(email)
-if size_name == 0 or size_email==0:
+size_phone  = len(phone)
+if size_name == 0 or size_email==0 or size_phone==0:
+
     print("the field can not be empty")
 else:
-    print("success")
+    #OV x = 1 and y = 9 || x and y = 0
+    if(size_name<3 or size_name>10): #nested condition
+        print("the name length must be minimum 3")
+    elif(size_phone!=11):
+        print("the phone number must have 11 digits")
+    else:
+        print("success")
