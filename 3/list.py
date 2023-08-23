9077461ad018d19c509ba86fa945f7cb4d22183e
salman <salmanmdsultan@gmail.com>
2023-08-24 01:04:50 +0600

    list done

---
+++ 3/list.py
@@ -0,0 +1,32 @@
+import random
+import numpy
+# cake_list = [0,1,2,3,4,5,6,7,8,9,10,10,10,10]
+# #list access - i) indexing ii) slicing
+# print(cake_list)
+# cake_list.append(11)
+# print(cake_list)
+
+# cake_list.extend([12,13,14,15])
+# print(cake_list)
+
+# cake_list.insert(0,25)
+# print(cake_list)
+
+# cake_list.remove(25)
+# print(cake_list)
+
+# cake_list.pop(2)
+# print(cake_list)
+
+# index = cake_list.index(15)
+# print(index)
+
+# count = cake_list.count(10)
+# print(count)
+
+random_data = list(numpy.random.randint(0, 10, size=100))
+
+list1 = [3,4,5,1]
+print((random_data))
+random_data.sort()
+print((random_data))
